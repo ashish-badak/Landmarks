d59e7d4a1c569a3065d543598d495a60acb1e754
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2025-05-03 15:36:00 +0530

    Embedded content in scroll view

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,30 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: .infinity)
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
+        ScrollView {
+            VStack {
+                MapView()
+                    .frame(height: 300)
+                CircleImage()
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                VStack(alignment: .leading) {
+                    Text("Turtle Rock")
+                        .font(.title)
+                    HStack {
+                        Text("Joshua Tree National Park")
+                        Spacer()
+                        Text("California")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("About Turtle Rock")
+                            .font(.title2)
+                        Text("Turtle Rock is a prominent rock formation in Joshua Tree National Park, California, known for its striking northwest face and the nearby picnic area. Popular with climbers and tourists, the area offers beginner-friendly climbing routes on its east face and hosts various events like stargazing and religious ceremonies.")
+                    }
                 }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider()
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("About Turtle Rock")
-                        .font(.title2)
-                    Text("Turtle Rock is a prominent rock formation in Joshua Tree National Park, California, known for its striking northwest face and the nearby picnic area. Popular with climbers and tourists, the area offers beginner-friendly climbing routes on its east face and hosts various events like stargazing and religious ceremonies.")
-                }
+                .padding()
             }
             .padding()
         }
