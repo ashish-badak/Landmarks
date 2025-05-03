48eafa56f64e846ee177201534a23adb94ebe01c
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2025-05-03 13:59:46 +0530

    Created CircleImage view

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ashish Badak on 03/05/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+#Preview {
+    CircleImage()
+}
