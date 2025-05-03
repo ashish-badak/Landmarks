f24f50ec41067e465c93ca5c1acfc628e7159758
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2025-05-03 15:22:45 +0530

    Added description and updated layout accordingly

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -15,20 +15,26 @@ struct ContentView: View {
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
-            VStack {
-                VStack(alignment: .leading) {
-                    Text("Turtle Rock")
-                        .font(.title)
-                    HStack {
-                        Text("Joshua Tree National Park")
-                        Spacer()
-                        Text("California")
-                    }
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
                 }
-                .padding()
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("About Turtle Rock")
+                        .font(.title2)
+                    Text("Turtle Rock is a prominent rock formation in Joshua Tree National Park, California, known for its striking northwest face and the nearby picnic area. Popular with climbers and tourists, the area offers beginner-friendly climbing routes on its east face and hosts various events like stargazing and religious ceremonies.")
+                }
             }
+            .padding()
         }
     }
 }
