81ccb2f0e52f76c1f2f48928b4120db391febfd0
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2025-05-03 19:43:07 +0530

    Created LandmarkRow view

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ashish Badak on 03/05/25.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    let landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            Image(landmark.imageName)
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
