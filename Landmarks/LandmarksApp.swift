83b779967611183bbd65e8358831664bb5e3d2dc
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2025-05-03 13:12:51 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ashish Badak on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
